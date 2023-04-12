170d7ff2143006b0057133933d01172b01cbe584
stone <4377040@qq.com>
2023-04-12 15:19:55 +0800

    ner初步测试完成.安装的新的python虚拟环境即可成功.

--- lab/uie/ner.py
+++ lab/uie/ner.py
@@ -5,3 +5,6 @@ schema = ['时间', '选手', '赛事名称'] # Define the schema for entity ext
 ie = Taskflow('information_extraction', schema=schema)
 pprint(ie("2月8日上午北京冬奥会自由式滑雪女子大跳台决赛中中国选手谷爱凌以188.25分获得金牌！")) # Better print results using pprint
 
+schema = ['肿瘤的大小', '肿瘤的个数', '肝癌级别', '脉管内癌栓分级']
+ie.set_schema(schema)
+pprint(ie("（右肝肿瘤）肝细胞性肝癌（II-III级，梁索型和假腺管型），肿瘤包膜不完整，紧邻肝被膜，侵及周围肝组织，未见脉管内癌栓（MVI分级：M0级）及卫星子灶形成。（肿物1个，大小4.2×4.0×2.8cm）。"))
